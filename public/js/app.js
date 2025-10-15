af68ddf7f09cab0ad1e1f10abff7fccb1a0928fe
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-10-15 22:24:00 +0000

    Correct typo in public/js/app.js comments

--- public/js/app.js
+++ public/js/app.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.classList.add('loading');
                 this.disabled = true;
 
-                // Add spniner
+                // Add spinner
                 const spinner = document.createElement('span');
                 spinner.className = 'loading-spinner inline-block w-4 h-4 ml-2';
                 this.appendChild(spinner);
