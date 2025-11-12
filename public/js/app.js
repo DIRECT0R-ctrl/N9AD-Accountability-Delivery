32eb4970afe626e26016a30b53db71b3736a1c33
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-11-12 00:40:00 +0000

    Reword comments in public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.classList.add('loading');
                 this.disabled = true;
 
-                // Add spinner
+                // Add sipnner
                 const spinner = document.createElement('span');
                 spinner.className = 'loading-spinner inline-block w-4 h-4 ml-2';
                 this.appendChild(spinner);
