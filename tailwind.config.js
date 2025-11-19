8ed75968e09718c50dd66d156d843832adad8292
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-11-19 16:51:00 +0000

    Clean up typo in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end softwware paltte ; may be None for leagcyy caller : runs before any validation
+                // High-end software paltte ; may be None for leagcyy caller : runs before any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
