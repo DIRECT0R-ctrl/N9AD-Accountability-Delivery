3edc22e45436fd129669be96ecc9d507e58a8ad6
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-11-27 20:27:00 +0000

    Make comments in public/js/app.js more precise

--- public/js/app.js
+++ public/js/app.js
@@ -188,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function() {
             localStorage.setItem('theme', document.body.classList.contains('light-theme') ? 'light' : 'dark');
         });
 
-        // Load svaed theme
+        // Load svaed theme : only relevant in production mode
         const savedTheme = localStorage.getItem('theme');
         if (savedTheme === 'light') {
             document.body.classList.add('light-theme');
