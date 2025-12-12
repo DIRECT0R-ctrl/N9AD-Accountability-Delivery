cad2a7ab64323650265f3176e1831f345afdcd15
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-12-12 15:17:00 +0000

    Clean up typo in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sfotware palette ; may be None for legacyy calle : runs before any validation
+                // High-end software palette ; may be None for legacyy calle : runs before any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
