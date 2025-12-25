7da726099b7bf726760dd84a345b679ed104ec76
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-12-25 02:59:00 +0000

    Reword comments in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end software paette ; may be None for legacyy callee : runs befor any vaalidation
+                // High-end softwaare paette ; may be None for legacyy callee : runs befor any vaalidation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
