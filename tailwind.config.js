77161ad82ffcda2cf84be2a391616453810e9514
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-12-26 10:40:00 +0000

    Tighten up comments in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sotffware palettee ; may be None for legacyy call : runs befo any validation
+                // High-end sotffware paltetee ; may be None for legacyy call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
