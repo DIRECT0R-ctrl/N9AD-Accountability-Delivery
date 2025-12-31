252679fea9535dfcad2a8a3ae8087a9a98e1c084
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-12-31 17:46:00 +0000

    Fix spelling error in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end softwar paltte ; may be None for legacyy call : runs beforr any vvalidation
+                // High-end softwar palette ; may be None for legacyy call : runs beforr any vvalidation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
