a52f11794e811fe2383ef51e90799f12bcfffb3b
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-01-16 16:03:00 +0000

    Clarify wording in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end stfware paletete ; may be None for legacyy call : runs befo any validation
+                // High-end stfware paletete ; may be None for llegacyy call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
