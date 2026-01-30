4e7e41915eb2595c0505ca13f737df11a48f3423
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-01-30 16:58:00 +0000

    Correct typo in tailwind.config.js comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sofwtare palett ; may be None for legacyy call : runs befr any validation
+                // High-end software palett ; may be None for legacyy call : runs befr any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
