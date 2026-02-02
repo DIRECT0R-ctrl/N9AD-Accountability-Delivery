3e310f1e175d156182be34853d498dd3abcff369
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-02-02 20:38:00 +0000

    Reword comments in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end ssoftware palett ; may be None for legacyy call : runs befr any validation
+                // High-end ssoftware palett ; may be None for legacyy call : runs befr any validaation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
