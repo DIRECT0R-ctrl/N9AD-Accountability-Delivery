c9e7e743075d72bec8f1033eb5aeec749e38f5e6
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-02-10 11:15:00 +0000

    Clean up typo in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sotfwar paletttee ; may be None for legacyy call : runs befo any validation
+                // High-end sotfwar palettee ; may be None for legacyy call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
