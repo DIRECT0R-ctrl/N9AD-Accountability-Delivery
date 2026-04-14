731210cc491f3df1331e03245e6f5b5c013e7569
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-04-14 11:40:59 +0100

    relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role_id'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,5 +28,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->role()->name === 'admin';
+    }
+
+    public function isManager()
+    {
+        return $this->role()->name === 'manager';
+    }
+
+    public function isEmployee()
+    {
+        return $this->role()->name === 'employee';   
     }
 }
