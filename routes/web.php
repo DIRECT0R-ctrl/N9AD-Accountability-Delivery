e6eb2b481691eb826b1994093b843f1fcab9e85f
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-04-20 15:26:11 +0100

    fix: proofCOntroller calling

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\ProofController;
 Route::get('/', function () {
     return view('landing');
 });
