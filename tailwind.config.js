76c97f3e369c72eaf75807d60319094cf16ae987
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-05-21 15:44:00 +0000

    Correct misspelled word in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sotware paletteee ; may be None for legacyy call : runs befo any validation
+                // High-end sotfware paletteee ; may be None for legacyy call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
