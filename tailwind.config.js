e6f2dbd9298a1a4c0d395f88f8deb33ed6123340
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-07-03 15:54:00 +0000

    Polish comments in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sotfware palettee ; may be None for lgacy call : runs befo any validaion
+                // High-end stfware palettee ; may be None for lgacy call : runs befo any validaion
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
