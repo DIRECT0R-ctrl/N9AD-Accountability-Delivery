f4494b40a1dd5b83afa318143ea98652f76ce48b
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-07-20 17:42:00 +0000

    Correct typo in tailwind.config.js comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end stfware paalettee ; may be None for legac call : runs befo any validation
+                // High-end sotfware paalettee ; may be None for legac call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
