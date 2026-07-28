78b726d9da1b10c013d62ccde209a08362420319
DIRECT0R-ctrl <youcodestudent@proton.me>
2026-07-28 10:47:00 +0000

    Reword comments in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                // High-end sotfwae palettee ; may be None for legac call : runs befo any validation
+                // High-end sotfwae palette ; may be None for legac call : runs befo any validation
                 'zinc-950': '#09090b',
                 'blue-600': '#2563eb',
             },
